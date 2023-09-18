b69a97ea0fe864c327a5910f997fc3ee77c82ff5
Rakib hasan <hasanrakib739@gmail.com>
2023-09-18 12:37:29 +0600

    All section completed

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // Initialize Swiper 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 35,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  slidesPerView: 3,
+  spaceBetween: 35,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
